f32a4f81e4f0d62102e2588c4c86a7e8c7e85a3c
Josh Ellinger <joshua.ellinger@lucidworks.com>
2025-12-04 20:39:04 -0800

    refactor: remove embedded stencil-grid-builder source code
    The StencilJS grid builder library has been moved to its own standalone repository
    at https://github.com/lucidworks/stencil-grid-builder
    
    All source code, tests, and configuration files have been removed from this POC repository.
    The production build output in stencil/ directory is retained for the GitHub Pages demo.
    
    This change simplifies the POC repository to focus on:
    - Vanilla JS implementation variants (left-top, transform, virtual, masonry)
    - Library integration examples (grapesjs, gridstack, moveable)
    - Deployed StencilJS demo (stencil/ build output only)
    
    For library development, see: https://github.com/lucidworks/stencil-grid-builder

--- stencil-grid-builder/testing/jest-setup.js
+++
@@ -1,6 +0,0 @@
-// Setup file to initialize the test environment
-// Using node environment - Stencil's testing library will create its own mock DOM
-
-// Load Stencil's Jest setup
-// This sets up the testing environment including mock-doc
-require('@stencil/core/testing/jest-setuptestframework.js');
